230ed1b24144ebf4d3e94c3e3bacb05adf175a93
Igor <igorrriwanow@yandex.ru>
2021-11-08 18:14:01 +0300

    did the 5 homeWork

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {TOGGLE_SHOW_PROFILE} from "../store/profile/action";
 
 const Profile = () => {
+    const dispatch = useDispatch();
+    const isShow = useSelector(state => state.isShow);
 
     return (
         <div>
             <h1>Профиль</h1>
+            <input type="checkbox" checked={isShow} onChange={()=>{
+                dispatch({type: TOGGLE_SHOW_PROFILE});
+                console.log(isShow);
+            }}/>
         </div>
     )
 }
 
-export default Profile
+export default Profile;
 
